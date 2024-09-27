190b3d2a10d1ab8a62b6f10a0e5d310199d8dddc
JasperF76 <jasjames122@gmail.com>
2024-09-27 16:02:57 -0400

    Finishing touches

--- src/client/components/About.jsx
+++ src/client/components/About.jsx
@@ -12,16 +12,16 @@ export default function About() {
                     <p>
                         If you've ever talked to me for more than 5 minutes, there's a good chance that you may already know that I spent nearly 3 decades of my working life as an arborist.
                         Climbing trees, pruning trees, cutting down trees, chipping up trees and even (occasionally!) planting trees has been, and will continue to be, a huge part of my life.
-                        Unfortunately, work as an arborist is not only very physically demanding, but also very dangerous. There are numerous ways to be injured doing my former job...
-                        And I went and discovered one of them. A neck injury sidelined me in 2022, and ultimately lead to me being unable to return to the profession that I'd dedicated more than half of my life to.
+                        However, work as an arborist is very physically demanding, and there are a number of ways to be injured doing my former job.
+                        A back injury had me off for a whole year in 2020, and then a neck injury sidelined me in 2022, and those two injuries ultimately lead to me being unable to return to the profession that I'd dedicated more than half of my life to.
                     </p>
 
                 </div>
 
                 <div className="paragraph_2">
                     <p>
-                        Which brings me to the creation of this website. Unable to continue trimming trees, I was compelled to find a brand new career at the tender age of 48 that my body could actually handle.
-                        And hopefully, fingers crossed, that new career is going to be as a software developer! After 3 months of very educational, very rewarding, and, at times, very <i>frustrating</i> instruction, I have managed to build my very first website.
+                        Which brings me to the creation of this website. Unable to continue my career as an arborist, I was left with no choice but to jump into a brand new career at the tender age of 48.
+                        And hopefully, fingers crossed, that new career is going to be as a software developer! After 3 months of very educational, very rewarding, and, at times, very <i>frustrating</i> instruction, I have managed to build this, my very first website.
                         When my FullStack Academy bootcamp instructor (hi Mark!) suggested that our final project should perhaps be something that we are passionate about, I knew right away that my capstone would have something to do with trees.
                     </p>
                     <img src={MeWithASaw} alt="Me with a saw" className='arborist_image' />
@@ -31,9 +31,9 @@ export default function About() {
                     <img src={MeMovingLogs} alt="Me on the log truck" className='arborist_image' />
                     <p>
                         And so I give you, "Trees of the World," a website dedicated to my love of trees, and that bridges my former profession to my new one.
-                        This site introduces you to some of the more iconic trees around the world, from the world's oldest known tree, Methuselah, to the largest tree, General Sherman.
-                        But don't forget about the ordinary, every day trees that you see in front of your house, or on your ride to work. Every tree is remarkable in it's own right, and they all speak to the brilliance of their Designer.
-                        My hope is that after viewing the amazing tree facts on this website, you'll come away with a newfound respect and appreciation for your silent neighbors, and for all that they do for us.
+                        This site introduces you to some of the more iconic trees around the world, from the world's oldest known tree, Methuselah, to the largest tree by volume, General Sherman.
+                        But don't forget about the ordinary, every day trees that you see in front of your house, or on your ride to work. Every tree is remarkable in it's own right, and their fascinating design speaks to the brilliance of their Designer.
+                        My hope is that after viewing the amazing trees on this website, you'll come away with a newfound respect and appreciation for your silent neighbors, and for all that they do for us.
                         And by creating and regularly updating this 'labor of love' project of mine, I can stay connected to the profession that I loved.
                     </p>
 
@@ -45,19 +45,19 @@ export default function About() {
                 <br /><br />
                 <div className='paragraph_4'>
                     <p>
-                        <strong>Special Thanks:</strong>
+                        <strong>Special Thanks to:</strong>
                         <br /><br />
                         <b><i>Jehovah God</i></b>, first and foremost, without whom I'm convinced I would not have made it this far
                         <br /><br />
-                        My loving wife and daughter, <i>Janine</i> and  <i>Jasmine</i>, for all their support, encouragement, and acceptance through the past couple of years of me navigating finding new employment
+                        My loving wife and daughter, <i>Janine</i> and  <i>Jasmine</i>, for all their support, encouragement, and acceptance through the past couple of years of me navigating finding new employment (and for talking me out of quitting coding a few days after starting javascript)
                         <br /><br />
                         <i>Marisa Brienza</i> and <i>Tami Glancy</i>, the amazing career counselors that steered me in the right direction
                         <br /><br />
                         <i>Louis Demicco</i>, <i>Thomas Hamilton</i>, and all the staff at my Dr/Physical Therapist
                         <br /><br />
-                        <i>Mark</i> (and <i>Lincoln</i>) <i>Lawrence</i>, the persons who taught me how to code this site
+                        <i>Mark</i> (and <i>Lincoln</i>) <i>Lawrence</i>, and the Fullstack mentors (shoutout to Caitlin and Javier!) the persons who taught me how to code this site
                         <br /><br />
-                        All my classmates at Fullstack Academy who had to put up with an old dog trying to learn a new trick
+                        My fellow classmates at Fullstack Academy for helping an old dog learn a few new tricks
                         <br /><br />
                         All the "tree dogs" I've worked with over the past 28 years, especially the ones we've lost along the way.
                     </p>
